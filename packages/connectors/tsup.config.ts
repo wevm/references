ab1da0654faf36b38bc69fc5d63f32d5d1314657
moxey.eth <jakemoxey@gmail.com>
2022-12-08 10:06:46 +1100

    feat: dev script

--- packages/connectors/tsup.config.ts
+++ packages/connectors/tsup.config.ts
@@ -4,6 +4,7 @@ import { getConfig } from '../../scripts/tsup'
 
 export default defineConfig(
   getConfig({
+    dev: process.env.DEV === 'true',
     entry: [
       'src/index.ts',
       'src/injected.ts',
