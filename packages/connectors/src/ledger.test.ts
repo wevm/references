3b7fbe484a986952761b280bed2b262306d966a1
moxey.eth <jakemoxey@gmail.com>
2022-12-12 08:22:05 +1100

    chore: add stub tests

--- packages/connectors/src/ledger.test.ts
+++ packages/connectors/src/ledger.test.ts
@@ -1,5 +1,32 @@
-import { describe, it } from 'vitest'
+import { testChains } from '@wagmi/core/internal/test'
+import { describe, expect, it } from 'vitest'
+
+import { LedgerConnector } from './ledger'
 
 describe('LedgerConnector', () => {
-  it.todo('inits')
+  it('inits', () => {
+    const connector = new LedgerConnector({
+      chains: testChains,
+      options: {},
+    })
+    expect(connector.name).toEqual('Ledger')
+  })
+
+  describe('behavior', () => {
+    it.todo('connects')
+
+    it.todo('disconnects via dapp (wagmi Connector)')
+
+    it.todo('disconnects via wallet (Ledger Live)')
+
+    it.todo('switch chains via dapp (wagmi Connector)')
+
+    it.todo('switch chains via wallet (Ledger Live)')
+
+    it.todo('switch accounts via wallet (Ledger Live)')
+
+    it.todo('sends a transaction')
+
+    it.todo('signs a message')
+  })
 })
