93040b89350f3d14211b60515773d2cf443ad41e
awkweb <tom@meagher.co>
2022-12-19 13:43:03 -0500

    build: generate exports (#24)

--- packages/chains/tsup.config.ts
+++ packages/chains/tsup.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'tsup'
 
 import { getConfig } from '../../scripts/tsup'
-import { exports } from './package.json'
 
 export default defineConfig(
   getConfig({
     dev: process.env.DEV === 'true',
     entry: ['src/index.ts'],
-    exports,
     platform: 'browser',
   }),
 )
