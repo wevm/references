822bc8845ea9ef2685d11c0b55dcc884d2914295
jxom <jakemoxey@gmail.com>
2022-12-28 06:55:30 +1100

    feat: WalletConnect v2 support (#19)
    * feat: WalletConnect v2 support
    
    Co-authored-by: Gancho Radkov <ganchoradkov93@gmail.com>
    
    * fix: check if chains are authorized
    
    * fix: assert provider.namespaces
    
    * tests: fix tests
    
    * fix: lint
    
    * chore: actions secret
    
    * refactor: pr review
    
    * refactor: guard provider decoration
    
    * chore: changeset
    
    Co-authored-by: Gancho Radkov <ganchoradkov93@gmail.com>

--- packages/connectors/src/walletConnect.test.ts
+++ packages/connectors/src/walletConnect.test.ts
@@ -1,8 +1,12 @@
-import { foundry } from '@wagmi/core/chains'
+import { foundry, goerli, mainnet } from '@wagmi/core/chains'
 import { testChains } from '@wagmi/core/internal/test'
+import WalletConnectProvider from '@walletconnect/ethereum-provider'
+import { UniversalProvider } from '@walletconnect/universal-provider'
 import { describe, expect, it } from 'vitest'
 
 import { WalletConnectConnector } from './walletConnect'
+
+const defaultChains = [mainnet, goerli]
 
 describe('WalletConnectConnector', () => {
   it('inits', () => {
@@ -15,5 +19,41 @@ describe('WalletConnectConnector', () => {
       },
     })
     expect(connector.name).toEqual('WalletConnect')
+  })
+
+  it('should use v1 by default', async () => {
+    const connector = new WalletConnectConnector({
+      chains: defaultChains,
+      options: {
+        rpc: {
+          [foundry.id]: foundry.rpcUrls.default.http[0]!,
+        },
+      },
+    })
+    expect(await connector.getProvider()).instanceOf(WalletConnectProvider)
+  })
+
+  it('should use v1 as configured by options', async () => {
+    const connector = new WalletConnectConnector({
+      chains: defaultChains,
+      options: {
+        rpc: {
+          [foundry.id]: foundry.rpcUrls.default.http[0]!,
+        },
+        version: '1',
+      },
+    })
+    expect(await connector.getProvider()).instanceOf(WalletConnectProvider)
+  })
+
+  it('should use v2 as configured by options', async () => {
+    const connector = new WalletConnectConnector({
+      chains: defaultChains,
+      options: {
+        projectId: process.env.VITE_WC_PROJECT_ID!,
+        version: '2',
+      },
+    })
+    expect(await connector.getProvider()).instanceOf(UniversalProvider)
   })
 })
