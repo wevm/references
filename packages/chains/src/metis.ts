ee2454bcb611f4c8cecf05b690ee08fc05a27deb
awkweb <tom@meagher.co>
2023-01-06 11:22:22 -0500

    Update packages/chains/src/metis.ts

--- packages/chains/src/metis.ts
+++ packages/chains/src/metis.ts
@@ -1,18 +1,21 @@
 import { Chain } from './types'
 
 export const metis: Chain = {
-    id: 1088,
+  id: 1_088,
+  name: 'Metis',
+  network: 'andromeda',
+  nativeCurrency: {
+    decimals: 18,
     name: 'Metis',
-    network: 'andromeda',
-    nativeCurrency: {
-        decimals: 18,
-        name: 'Metis',
-        symbol: 'METIS',
+    symbol: 'METIS',
+  },
+  rpcUrls: {
+    default: { http: ['https://andromeda.metis.io/?owner=1088'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Andromeda Explorer',
+      url: 'https://andromeda-explorer.metis.io',
     },
-    rpcUrls: {
-        default: { http: ['https://andromeda.metis.io/?owner=1088'] },
-    },
-    blockExplorers: {
-        default: { name: 'Andromeda Explorer', url: 'https://andromeda-explorer.metis.io' },
-    },
+  },
 }
