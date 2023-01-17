77af8ee15b7cf8db76d9224bacd1d0098b219bce
Songkeys <songv587@gmail.com>
2023-01-17 08:11:16 +0800

    chore: lint

--- packages/chains/src/crossbell.ts
+++ packages/chains/src/crossbell.ts
@@ -1,22 +1,20 @@
 import { Chain } from './types'
 
 export const crossbell: Chain = {
-	id: 3737,
-	network: "crossbell",
-	name: "Crossbell",
-	nativeCurrency: {
-		decimals: 18,
-		name: "CSB",
-		symbol: "CSB",
-	},
-	rpcUrls: {
-		default: {
-			http: ["https://rpc.crossbell.io"],
-		},
-	},
-	blockExplorers: {
-		default: { name: "CrossScan", url: "https://scan.crossbell.io" },
+  id: 3737,
+  network: 'crossbell',
+  name: 'Crossbell',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'CSB',
+    symbol: 'CSB',
   },
-};
-
-
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.crossbell.io'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'CrossScan', url: 'https://scan.crossbell.io' },
+  },
+}
