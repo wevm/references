cfbaf6163b01474c90691d06344e12e396d09548
neil. {} <105974692+neil0x46dc@users.noreply.github.com>
2023-01-21 21:06:11 +0800

    feat: add filecoin testnet

---
+++ packages/chains/src/filecoinHyperspace.ts
@@ -0,0 +1,26 @@
+import { Chain } from './types'
+
+export const filecoin: Chain = {
+  id: 3141,
+  name: "Filecoin",
+  network: "hyperspace-testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Test FIL",
+    symbol: "TFIL",
+  },
+  rpcUrls: {
+    default: { http: ["https://api.hyperspace.node.glif.io/rpc/v1"] },
+    public: { http: ["https://api.hyperspace.node.glif.io/rpc/v1"] },
+  },
+  blockExplorers: {
+    gilf: {
+      name: "Gilf",
+      url: "https://explorer.glif.io/?network=hyperspace",
+    },
+    default: {
+      name: "FilFox",
+      url: "https://hyperspace.filfox.info/en",
+    },
+  },
+};
