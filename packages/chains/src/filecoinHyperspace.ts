a6092fc48354f67dfbba864a474d22ee51bc41c1
neil. {} <105974692+neil0x46dc@users.noreply.github.com>
2023-01-21 21:45:40 +0800

    feat: add filecoin hyperspace testnet

--- packages/chains/src/filecoinHyperspace.ts
+++ packages/chains/src/filecoinHyperspace.ts
@@ -2,25 +2,19 @@ import { Chain } from './types'
 
 export const filecoin: Chain = {
   id: 3141,
-  name: "Filecoin Hyperspace",
-  network: "filecoin-hyperspace",
+  name: 'Filecoin Hyperspace',
+  network: 'filecoin-hyperspace',
   nativeCurrency: {
     decimals: 18,
-    name: "Test FIL",
-    symbol: "TFIL",
+    name: 'testnet filecoin',
+    symbol: 'tFIL',
   },
   rpcUrls: {
-    default: { http: ["https://api.hyperspace.node.glif.io/rpc/v1"] },
-    public: { http: ["https://api.hyperspace.node.glif.io/rpc/v1"] },
+    default: { http: ['https://api.hyperspace.node.glif.io/rpc/v1'] },
+    public: { http: ['https://api.hyperspace.node.glif.io/rpc/v1'] },
   },
   blockExplorers: {
-    gilf: {
-      name: "Gilf",
-      url: "https://explorer.glif.io/?network=hyperspace",
-    },
-    default: {
-      name: "FilFox",
-      url: "https://hyperspace.filfox.info/en",
-    },
+    default: { name: 'FilFox', url: 'https://hyperspace.filfox.info/en',},
+    gilf: { name: 'Gilf', url: 'https://explorer.glif.io/?network=hyperspace',},
   },
 };
