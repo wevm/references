75f652cc3a230fd3eefd07c6f8681efc78a1c13e
Thomas Bush <bushthomas@hotmail.com>
2023-01-23 09:43:39 +0100

    Add Canto network support

---
+++ packages/chains/src/canto.ts
@@ -0,0 +1,19 @@
+import { Chain } from './types'
+
+export const bsc: Chain = {
+  id: 7700,
+  name: 'Canto',
+  network: 'canto',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Canto',
+    symbol: 'CANTO',
+  },
+  rpcUrls: {
+    default: { http: ['https://canto.slingshot.finance'] },
+    public: { http: ['https://canto.slingshot.finance'] },
+  },
+  blockExplorers: {
+    default: { name: 'Canto EVM Explorer (Blockscout)', url: 'https://evm.explorer.canto.io' },
+  },
+}
