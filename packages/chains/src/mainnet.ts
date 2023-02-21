9ba042f10df55ee8225dc67a037110c18a68cb7a
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-02-20 22:45:15 -0500

    Update mainnet.ts

--- packages/chains/src/mainnet.ts
+++ packages/chains/src/mainnet.ts
@@ -1,47 +1,26 @@
 import { Chain } from './types'
 
 export const mainnet = {
-  id: 1,
-  network: 'homestead',
+  id: 2330,
+//  network: 'homestead',
   name: 'Ethereum',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  nativeCurrency: { name: 'Altcoinchain', symbol: 'ALT', decimals: 18 },
   rpcUrls: {
-    alchemy: {
-      http: ['https://eth-mainnet.g.alchemy.com/v2'],
-      webSocket: ['wss://eth-mainnet.g.alchemy.com/v2'],
-    },
-    infura: {
-      http: ['https://mainnet.infura.io/v3'],
-      webSocket: ['wss://mainnet.infura.io/ws/v3'],
-    },
     default: {
-      http: ['https://cloudflare-eth.com'],
+      http: ['https://rpc0.altcoinchain.org/rpc'],
     },
     public: {
-      http: ['https://cloudflare-eth.com'],
+      http: ['https://rpc0.altcoinchain.org/rpc'],
     },
   },
   blockExplorers: {
     etherscan: {
       name: 'Etherscan',
-      url: 'https://etherscan.io',
+      url: 'http://expedition.altcoinchain.org',
     },
     default: {
       name: 'Etherscan',
-      url: 'https://etherscan.io',
-    },
-  },
-  contracts: {
-    ensRegistry: {
-      address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
-    },
-    ensUniversalResolver: {
-      address: '0x74E20Bd2A1fE0cdbe45b9A1d89cb7e0a45b36376',
-      blockCreated: 16172161,
-    },
-    multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
-      blockCreated: 14353601,
+      url: 'http://expedition.altcoinchain.org',
     },
   },
 } as const satisfies Chain
