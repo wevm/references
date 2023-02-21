76d659d60594914333169c4fdfd877df5cfdbce7
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-02-20 22:43:24 -0500

    Update polygonMumbai.ts

--- packages/chains/src/polygonMumbai.ts
+++ packages/chains/src/polygonMumbai.ts
@@ -1,41 +1,28 @@
 import { Chain } from './types'
 
 export const polygonMumbai = {
-  id: 80_001,
+  id: 2330,
   name: 'Polygon Mumbai',
   network: 'maticmum',
-  nativeCurrency: { name: 'MATIC', symbol: 'MATIC', decimals: 18 },
+  nativeCurrency: { name: 'Altcoinchain', symbol: 'ALT', decimals: 18 },
   rpcUrls: {
-    alchemy: {
-      http: ['https://polygon-mumbai.g.alchemy.com/v2'],
-      webSocket: ['wss://polygon-mumbai.g.alchemy.com/v2'],
-    },
-    infura: {
-      http: ['https://polygon-mumbai.infura.io/v3'],
-      webSocket: ['wss://polygon-mumbai.infura.io/ws/v3'],
-    },
     default: {
-      http: ['https://matic-mumbai.chainstacklabs.com'],
+      http: ['https://rpc0.altcoinchain.org/rpc'],
     },
     public: {
-      http: ['https://matic-mumbai.chainstacklabs.com'],
+      http: ['https://rpc0.altcoinchain.org/rpc'],
     },
   },
   blockExplorers: {
     etherscan: {
       name: 'PolygonScan',
-      url: 'https://mumbai.polygonscan.com',
+      url: 'http://expedition.altcoinchain.org/',
     },
     default: {
       name: 'PolygonScan',
-      url: 'https://mumbai.polygonscan.com',
+      url: 'http://expedition.altcoinchain.org/',
     },
   },
-  contracts: {
-    multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
-      blockCreated: 25770160,
-    },
-  },
-  testnet: true,
+
+  testnet: false,
 } as const satisfies Chain
