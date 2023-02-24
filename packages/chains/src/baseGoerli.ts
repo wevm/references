d04aa1b1bdd1bb6d867fced79f6deedd91542692
jxom <jakemoxey@gmail.com>
2023-02-24 12:10:18 +1100

    feat(chains): added base goerli (#127)
    * feat(chains): added base goerli
    
    * Update baseGoerli.ts

---
+++ packages/chains/src/baseGoerli.ts
@@ -0,0 +1,27 @@
+import { Chain } from './types'
+
+export const baseGoerli = {
+  id: 84531,
+  network: 'base-goerli',
+  name: 'Base Goerli',
+  nativeCurrency: { name: 'Base Goerli', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://goerli.base.org'],
+    },
+    public: {
+      http: ['https://goerli.base.org'],
+    },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'Basescan',
+      url: 'https://goerli.basescan.org',
+    },
+    default: {
+      name: 'Basescan',
+      url: 'https://goerli.basescan.org',
+    },
+  },
+  testnet: true,
+} as const satisfies Chain
