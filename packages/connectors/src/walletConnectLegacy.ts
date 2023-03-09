e3a3fee3779863116bc0761de4a141942486b40b
Spencer Yen <spencersyen@gmail.com>
2023-03-09 16:13:24 -0500

    Add Uniswap Wallet to WalletConnect switch chain regex (#151)

--- packages/connectors/src/walletConnectLegacy.ts
+++ packages/connectors/src/walletConnectLegacy.ts
@@ -18,8 +18,10 @@ import { Connector } from './base'
  * - MetaMask (metamask.io)
  * - Rainbow (rainbow.me)
  * - Trust Wallet (trustwallet.com)
+ * - Uniswap Wallet (uniswap.org)
  */
-const switchChainAllowedRegex = /(imtoken|metamask|rainbow|trust wallet)/i
+const switchChainAllowedRegex =
+  /(imtoken|metamask|rainbow|trust wallet|uniswap wallet)/i
 
 type WalletConnectOptions = ConstructorParameters<
   typeof WalletConnectProvider
