4cd19506fdff4e74afdf156db9cdf4b36493965b
TheGreatAxios <sawyer@dirtroad.dev>
2023-03-10 07:57:49 -0800

    Added Nebula Gaming Hub Testnet

---
+++ packages/chains/src/skale/nebulaTestnet.ts
@@ -0,0 +1,27 @@
+import { Chain } from '../types'
+
+export const skaleNebulaTestnet = {
+  id: 503_129_905,
+  name: 'SKALE | Nebula Gaming Hub Testnet',
+  network: 'skale-nebula-testnet',
+  nativeCurrency: { name: 'sFUEL', symbol: 'sFUEL', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://staging-v3.skalenodes.com/v1/staging-faint-slimy-achird'],
+    },
+    public: {
+      http: ['https://staging-v3.skalenodes.com/v1/staging-faint-slimy-achird'],
+    },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'Blockscout',
+      url: 'https://staging-faint-slimy-achird.explorer.staging-v3.skalenodes.com',
+    },
+    default: {
+      name: 'Blockscout',
+      url: 'https://staging-faint-slimy-achird.explorer.staging-v3.skalenodes.com',
+    },
+  },
+  contracts: {},
+} as const satisfies Chain
