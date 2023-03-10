396cb08bf54a49352410c48b89416c44f450a915
TheGreatAxios <sawyer@dirtroad.dev>
2023-03-10 08:03:24 -0800

    Added Titan Community Hub Testnet

---
+++ packages/chains/src/skale/titanTestnet.ts
@@ -0,0 +1,31 @@
+import { Chain } from '../types'
+
+export const skaleTitanTestnet = {
+  id: 1_517_929_550,
+  name: 'SKALE | Titan Community Hub Testnet',
+  network: 'skale-titan-testnet',
+  nativeCurrency: { name: 'sFUEL', symbol: 'sFUEL', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: [
+        'https://staging-v3.skalenodes.com/v1/staging-aware-chief-gianfar',
+      ],
+    },
+    public: {
+      http: [
+        'https://staging-v3.skalenodes.com/v1/staging-aware-chief-gianfar',
+      ],
+    },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'Blockscout',
+      url: 'https://staging-aware-chief-gianfar.explorer.staging-v3.skalenodes.com',
+    },
+    default: {
+      name: 'Blockscout',
+      url: 'https://staging-aware-chief-gianfar.explorer.staging-v3.skalenodes.com',
+    },
+  },
+  contracts: {},
+} as const satisfies Chain
