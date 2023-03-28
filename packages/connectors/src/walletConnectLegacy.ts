9f8c5392b6e9b861a55c5f0537953b76a22dee07
jxom <jakemoxey@gmail.com>
2023-03-28 13:47:04 +1100

    fix: programmatically set http provider for `WalletConnectLegacyProvider` (#196)
    * fix: programmatically set http provider
    
    * Update walletConnectLegacy.ts
    
    * fix: programmatically set http provider

--- packages/connectors/src/walletConnectLegacy.ts
+++ packages/connectors/src/walletConnectLegacy.ts
@@ -139,6 +139,16 @@ export class WalletConnectLegacyConnector extends Connector<
         chainId,
         rpc: { ...rpc, ...this.options?.rpc },
       })
+
+      // `@walletconnect/legacy-provider` automatically sets the chainId to `1`
+      // if a wallet does not support the target chain regardless of what `chainId`
+      // we pass to `WalletConnectProvider`.
+      // This causes the target chain RPC URL to become out-of-sync.
+      // WalletConnect's HTTP Provider should still work even if a wallet does
+      // not support the chain. We just need to provide it with a valid RPC URL.
+      // Here, we are making sure the RPC URL is set to the chain's RPC URL.
+      // @ts-expect-error – ignore
+      this.#provider.http = await this.#provider.setHttpProvider(chainId)
     }
 
     return this.#provider
