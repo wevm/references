9de63cd59f8062e9a2e16ca5fe32632123eeecf9
manny <318082+codingwithmanny@users.noreply.github.com>
2023-03-28 20:42:41 -0700

    feat: add Polygon zkEVM Mainnet (#201)
    * feat: Added Polygon zkEVM Mainnet
    
    Added new network and rpc credentials for Polygon zkEVM Mainnet
    
    BREAKING CHANGE: No
    
    * fix: Removed testnet
    
    Removed testnet flag from RPC details
    
    BREAKING CHANGE: No
    
    * Update polygonZkEvm.ts
    
    * Create .changeset/clean-ducks-hide.md
    
    ---------
    
    Co-authored-by: codingwithmanny <manny@MBPM1MAX.local>
    Co-authored-by: jxom <jakemoxey@gmail.com>

---
+++ .changeset/clean-ducks-hide.md
@@ -0,0 +1,5 @@
+---
+"@wagmi/chains": patch
+---
+
+Added Polygon zkEVM Mainnet
---
+++ packages/chains/src/polygonZkEvm.ts
@@ -0,0 +1,22 @@
+import { Chain } from './types'
+
+export const polygonZkEvm = {
+  id: 1101,
+  name: 'Polygon zkEVM',
+  network: 'polygon-zkevm',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://zkevm-rpc.com'],
+    },
+    public: {
+      http: ['https://zkevm-rpc.com'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'PolygonScan',
+      url: 'https://zkevm.polygonscan.com',
+    },
+  },
+} as const satisfies Chain
