65b34a6afdadb4804d9556baf437797158100468
Saviour1001 <harshghodkar@gmail.com>
2023-04-05 20:06:01 +0530

    Add Linea Testnet Support

---
+++ packages/chains/src/lineaTestnet.ts
@@ -0,0 +1,25 @@
+import { Chain } from './types'
+
+export const lineaTestnet = {
+  id: 59140,
+  name: 'Linea Testnet',
+  network: 'linea-testnet',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.goerli.linea.build/'],
+      webSocket: ['wss://rpc.goerli.linea.build/'],
+    },
+    public: {
+      http: ['https://rpc.goerli.linea.build/'],
+      webSocket: ['wss://rpc.goerli.linea.build/'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'BlockScout',
+      url: 'https://explorer.goerli.linea.build/',
+    },
+  },
+  testnet: true,
+} as const satisfies Chain
