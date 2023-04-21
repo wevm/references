5d9bcf3caec95b198892bf0f1eef40f37e6b0f9f
refined-x <tower1229@foxmail.com>
2023-04-21 20:03:09 +0800

    fix: update contracts

--- packages/chains/src/syscoin.ts
+++ packages/chains/src/syscoin.ts
@@ -19,8 +19,8 @@ export const syscoin = {
   },
   contracts: {
     multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
-      blockCreated: 268670,
+      address: '0x000562033783B1136159E10d976B519C929cdE8e',
+      blockCreated: 80637,
     },
   },
 } as const satisfies Chain
