d32e1b062c183f7b8e303a3acb5fc5fc532b1f2d
Marco Monaco <monaco.marco83@gmail.com>
2023-04-21 18:57:03 +0200

    Update linea (#239)
    * Updated Linea Testnet with Infura endpoint and Multicall3
    
    * Updated Linea Testnet with Infura endpoint and Multicall3
    
    * Updated Linea Testnet with Infura endpoint and Multicall3
    
    * Update lineaTestnet.ts
    
    * chore: format
    
    ---------
    
    Co-authored-by: jxom <jakemoxey@gmail.com>
    Co-authored-by: Tom Meagher <tom@meagher.co>

--- packages/chains/src/lineaTestnet.ts
+++ packages/chains/src/lineaTestnet.ts
@@ -1,11 +1,15 @@
 import { Chain } from './types'
 
 export const lineaTestnet = {
-  id: 59140,
-  name: 'Linea Testnet',
+  id: 59_140,
+  name: 'Linea Goerli Testnet',
   network: 'linea-testnet',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  nativeCurrency: { name: 'Linea Ether', symbol: 'ETH', decimals: 18 },
   rpcUrls: {
+    infura: {
+      http: ['https://consensys-zkevm-goerli-prealpha.infura.io/v3'],
+      webSocket: ['wss://consensys-zkevm-goerli-prealpha.infura.io/ws/v3'],
+    },
     default: {
       http: ['https://rpc.goerli.linea.build'],
       webSocket: ['wss://rpc.goerli.linea.build'],
@@ -19,6 +23,12 @@ export const lineaTestnet = {
     default: {
       name: 'BlockScout',
       url: 'https://explorer.goerli.linea.build',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 498623,
     },
   },
   testnet: true,
