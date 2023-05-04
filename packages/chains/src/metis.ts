88ab6e99600de894be9493b9db1066e0b8a79722
15077693d <oscaryiu.lapsang@gmail.com>
2023-05-04 11:55:37 +0800

    update signle quotations

--- packages/chains/src/metis.ts
+++ packages/chains/src/metis.ts
@@ -1,28 +1,28 @@
-import { Chain } from "./types";
+import { Chain } from './types'
 
 export const metis = {
   id: 1_088,
-  name: "Metis",
-  network: "andromeda",
+  name: 'Metis',
+  network: 'andromeda',
   nativeCurrency: {
     decimals: 18,
-    name: "Metis",
-    symbol: "METIS",
+    name: 'Metis',
+    symbol: 'METIS',
   },
   rpcUrls: {
-    default: { http: ["https://andromeda.metis.io/?owner=1088"] },
-    public: { http: ["https://andromeda.metis.io/?owner=1088"] },
+    default: { http: ['https://andromeda.metis.io/?owner=1088'] },
+    public: { http: ['https://andromeda.metis.io/?owner=1088'] },
   },
   blockExplorers: {
     default: {
-      name: "Andromeda Explorer",
-      url: "https://andromeda-explorer.metis.io",
+      name: 'Andromeda Explorer',
+      url: 'https://andromeda-explorer.metis.io',
     },
   },
   contracts: {
     multicall3: {
-      address: "0xca11bde05977b3631167028862be2a173976ca11",
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
       blockCreated: 2338552,
     },
   },
-} as const satisfies Chain;
+} as const satisfies Chain
