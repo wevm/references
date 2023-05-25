20cc41571483fb20b00609b21f5f570269475fee
Volchunovich <volchunovich.artem@gmail.com>
2023-05-25 18:21:30 +0300

    rollback getAccounts changes

--- packages/connectors/src/mock/provider.ts
+++ packages/connectors/src/mock/provider.ts
@@ -49,8 +49,7 @@ export class MockProvider {
   }
 
   async getAccounts() {
-    const walletClient = this.#walletClient
-    const address = walletClient?.account.address
+    const address = this.#walletClient?.account.address
     if (!address) return []
     return [getAddress(address)]
   }
