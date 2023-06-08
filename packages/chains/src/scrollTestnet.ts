6e19f20db29fb46c5370039620010bf2cb436e10
AtelyPham <tinpham1412000@gmail.com>
2023-06-08 12:57:36 +0700

    chore: add multicall3 for scroll testnet

--- packages/chains/src/scrollTestnet.ts
+++ packages/chains/src/scrollTestnet.ts
@@ -22,4 +22,10 @@ export const scrollTestnet = {
     },
   },
   testnet: true,
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 2745641,
+    },
+  },
 } as const satisfies Chain
