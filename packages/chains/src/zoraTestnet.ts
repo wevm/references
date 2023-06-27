fa0a4724bea13ed927c4f523ca7e4ce2d7c83383
Jackie Luo <jackie@ourzora.com>
2023-06-27 10:52:59 -0400

    update zora chain details

--- packages/chains/src/zoraTestnet.ts
+++ packages/chains/src/zoraTestnet.ts
@@ -2,26 +2,26 @@ import { Chain } from './types'
 
 export const zoraTestnet = {
   id: 999,
-  name: 'ZORA Goerli Testnet',
+  name: 'Zora Goerli Testnet',
   network: 'zora-testnet',
   nativeCurrency: {
     decimals: 18,
-    name: 'ZORA Goerli',
+    name: 'Zora Goerli',
     symbol: 'ETH',
   },
   rpcUrls: {
     default: {
-      http: ['https://testnet.rpc.zora.co'],
-      webSocket: ['wss://testnet.rpc.zora.co'],
+      http: ['https://testnet.rpc.zora.energy'],
+      webSocket: ['wss://testnet.rpc.zora.energy'],
     },
     public: {
-      http: ['https://testnet.rpc.zora.co'],
-      webSocket: ['wss://testnet.rpc.zora.co'],
+      http: ['https://testnet.rpc.zora.energy'],
+      webSocket: ['wss://testnet.rpc.zora.energy'],
     },
   },
   blockExplorers: {
-    etherscan: { name: 'ZORA', url: 'https://testnet.explorer.zora.co' },
-    default: { name: 'ZORA', url: 'https://testnet.explorer.zora.co' },
+    etherscan: { name: 'Zora', url: 'https://testnet.explorer.zora.energy' },
+    default: { name: 'Zora', url: 'https://testnet.explorer.zora.energy' },
   },
   contracts: {
     multicall3: {
