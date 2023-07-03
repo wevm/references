ff538574b7f21dcd8c6dc9080b92095b2a6b8f15
oxSaturn <oxSaturn@proton.me>
2023-07-03 10:26:03 +0800

    fix: import EthereumProvider instead of default (#382)
    * fix: import EthereumProvider instead of default
    
    * Update violet-days-arrive.md
    
    ---------
    
    Co-authored-by: jxom <jakemoxey@gmail.com>

---
+++ .changeset/violet-days-arrive.md
@@ -0,0 +1,5 @@
+---
+"@wagmi/connectors": patch
+---
+
+Fixed issue importing `EthereumProvider` in Vite environments.
--- packages/connectors/src/walletConnect.ts
+++ packages/connectors/src/walletConnect.ts
@@ -284,11 +284,8 @@ export class WalletConnectConnector extends Connector<
   }
 
   async #initProvider() {
-    const {
-      default: EthereumProvider,
-      OPTIONAL_EVENTS,
-      OPTIONAL_METHODS,
-    } = await import('@walletconnect/ethereum-provider')
+    const { EthereumProvider, OPTIONAL_EVENTS, OPTIONAL_METHODS } =
+      await import('@walletconnect/ethereum-provider')
     const [defaultChain, ...optionalChains] = this.chains.map(({ id }) => id)
     if (defaultChain) {
       const {
