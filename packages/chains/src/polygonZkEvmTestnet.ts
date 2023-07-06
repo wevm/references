0a6e6daa355ea0540ccf0aaf913f4efb120fce76
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-07-07 04:12:39 +0800

    feat: add multicall3 to Polygon zkEVM (#408)

--- packages/chains/src/polygonZkEvmTestnet.ts
+++ packages/chains/src/polygonZkEvmTestnet.ts
@@ -14,10 +14,20 @@ export const polygonZkEvmTestnet = {
     },
   },
   blockExplorers: {
-    default: {
+    blockscout: {
       name: 'Blockscout',
       url: 'https://explorer.public.zkevm-test.net',
     },
+    default: {
+      name: 'PolygonScan',
+      url: 'https://testnet-zkevm.polygonscan.com',
+    },
   },
   testnet: true,
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 525686,
+    },
+  },
 } as const satisfies Chain
