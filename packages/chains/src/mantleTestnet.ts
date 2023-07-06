4b411d2173d9ac4f18d3a8732159b604e1a59f99
Dmitrii Noskov <dmitrynoskovcz@gmail.com>
2023-07-06 22:04:11 +0200

    feat: add Mantle and Mantle testnet (#399)

---
+++ packages/chains/src/mantleTestnet.ts
@@ -0,0 +1,27 @@
+import { Chain } from './types'
+
+export const mantleTestnet = {
+  id: 5001,
+  name: 'Mantle Testnet',
+  network: 'mantle',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'MNT',
+    symbol: 'MNT',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.testnet.mantle.xyz'] },
+    public: { http: ['https://rpc.testnet.mantle.xyz'] },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'Mantle Testnet Explorer',
+      url: 'https://explorer.testnet.mantle.xyz',
+    },
+    default: {
+      name: 'Mantle Testnet Explorer',
+      url: 'https://explorer.testnet.mantle.xyz',
+    },
+  },
+  testnet: true,
+} as const satisfies Chain
