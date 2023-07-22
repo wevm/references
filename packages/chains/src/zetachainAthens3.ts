cad66c1c13467bc80ffd42dce671432ed85b6da4
Syed Ahkam <smahkam57@gmail.com>
2023-07-23 05:25:04 +0530

    chore: formatter

--- packages/chains/src/zetachainAthens3.ts
+++ packages/chains/src/zetachainAthens3.ts
@@ -2,26 +2,26 @@ import { Chain } from './types'
 
 export const zetachainAthens3 = {
   id: 7001,
-  name: "ZetaChain Athens 3",
-  network: "zetachain-athens-evm",
+  name: 'ZetaChain Athens 3',
+  network: 'zetachain-athens-evm',
   nativeCurrency: {
     decimals: 18,
-    name: "Zeta",
-    symbol: "aZETA",
+    name: 'Zeta',
+    symbol: 'aZETA',
   },
   rpcUrls: {
     public: {
-      http: ["https://zetachain-athens-evm.blockpi.network/v1/rpc/public"],
+      http: ['https://zetachain-athens-evm.blockpi.network/v1/rpc/public'],
     },
     default: {
-      http: ["https://zetachain-athens-evm.blockpi.network/v1/rpc/public"],
+      http: ['https://zetachain-athens-evm.blockpi.network/v1/rpc/public'],
     },
   },
   blockExplorers: {
     default: {
-      name: "ZetaScan",
-      url: "https://athens3.explorer.zetachain.com"
-    }
+      name: 'ZetaScan',
+      url: 'https://athens3.explorer.zetachain.com',
+    },
   },
   testnet: true,
-} as const satisfies Chain;
+} as const satisfies Chain
