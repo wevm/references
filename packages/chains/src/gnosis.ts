0c5a32bcc823c679a2b4c04efb6038bedd91e9a5
Major <90963895+Majorfi@users.noreply.github.com>
2023-08-09 06:12:26 +0200

    feat: add multicall3 for gnosis chain (#456)
    * feat: add multicall3 for gnosis chain
    
    * Chores: add changeset
    
    * Update lovely-keys-train.md
    
    ---------
    
    Co-authored-by: jxom <jakemoxey@gmail.com>

--- packages/chains/src/gnosis.ts
+++ packages/chains/src/gnosis.ts
@@ -23,4 +23,10 @@ export const gnosis = {
       url: 'https://blockscout.com/xdai/mainnet',
     },
   },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 21022491,
+    },
+  },
 } as const satisfies Chain
