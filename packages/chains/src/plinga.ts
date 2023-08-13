d957506a9d275bd009d0e44831c5476692b2c78b
git <eae966@gmail.com>
2023-08-13 17:24:44 +0330

    Multicall has been updated.

--- packages/chains/src/plinga.ts
+++ packages/chains/src/plinga.ts
@@ -1,26 +1,26 @@
-import { Chain } from './types'
+import { Chain } from "./types";
 
 export const plinga = {
   id: 242,
-  name: 'Plinga AI Chain',
-  network: 'plinga',
+  name: "Plinga AI Chain",
+  network: "plinga",
   nativeCurrency: {
     decimals: 18,
-    name: 'Plinga',
-    symbol: 'PLINGA',
+    name: "Plinga",
+    symbol: "PLINGA",
   },
   rpcUrls: {
-    default: { http: ['https://rpcurl.mainnet.plgchain.com'] },
-    public: { http: ['https://rpcurl.mainnet.plgchain.com'] },
+    default: { http: ["https://rpcurl.mainnet.plgchain.com"] },
+    public: { http: ["https://rpcurl.mainnet.plgchain.com"] },
   },
   blockExplorers: {
-    etherscan: { name: 'PlgScan', url: 'https://www.plgscan.com' },
-    default: { name: 'PlgScan', url: 'https://www.plgscan.com' },
+    etherscan: { name: "PlgScan", url: "https://www.plgscan.com" },
+    default: { name: "PlgScan", url: "https://www.plgscan.com" },
   },
   contracts: {
     multicall3: {
-      address: '0x45936F98C594D8ed17b1D895162797BD2E774b45',
-      blockCreated: 5044,
+      address: "0x0989576160f2e7092908BB9479631b901060b6e4",
+      blockCreated: 204489,
     },
   },
-} as const satisfies Chain
+} as const satisfies Chain;
