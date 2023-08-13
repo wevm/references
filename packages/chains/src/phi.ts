a71f47284fe2c16d6c2c3c38f499c7e11e51abce
jxom <jakemoxey@gmail.com>
2023-08-13 21:05:30 +0200

    Update phi.ts

--- packages/chains/src/phi.ts
+++ packages/chains/src/phi.ts
@@ -2,18 +2,10 @@ import { Chain } from './types'
 
 export const phi = {
   id: 144,
-  name: 'Phi',
+  name: 'PHI Network v2',
   network: 'PHI',
   nativeCurrency: { name: 'PHI', symbol: 'PHI', decimals: 18 },
   rpcUrls: {
-    alchemy: {
-      http: ['https://connect.phi.network'],
-      webSocket: ['wss://connect.phi.network],
-    },
-    infura: {
-      http: ['https://connect.phi.network'],
-      webSocket: ['wss://connect.phi.network'],
-    },
     default: {
       http: ['https://connect.phi.network'],
     },
@@ -30,11 +22,5 @@ export const phi = {
       name: 'Phiscan',
       url: 'https://phiscan.com',
     },
-  },
-  contracts: {
-    multicall3: {
-      address: '0xc2f41B404a6757863AAeF49ff93039421acCd630',
-      blockCreated: 360030,
-    },
-  },
+  }
 } as const satisfies Chain
