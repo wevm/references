906020a6627508e5ef82611ea055096f5e5a86da
Goraniya Karan <karangoraniya@gmail.com>
2023-08-25 18:26:07 +0530

    feat: add opBNB testnet multicall3 contract address 🚀

--- packages/chains/src/opBNBTestnet.ts
+++ packages/chains/src/opBNBTestnet.ts
@@ -15,8 +15,14 @@ export const opBNBTestnet = {
         default: { http: ['https://opbnb-testnet-rpc.bnbchain.org'] },
     },
     blockExplorers: {
-        etherscan: { name: 'opbnbscan', url: 'https://opbnb-testnet.bscscan.com/' },
-        default: { name: 'opbnbscan', url: 'https://opbnbscan.com/' },
+        etherscan: { name: 'opbnbscan', url: 'https://opbnb-testnet.bscscan.com' },
+        default: { name: 'opbnbscan', url: 'https://opbnbscan.com' },
     },
+    contracts: {
+        multicall3: {
+          address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+          blockCreated: 3705108,
+        },
+      },
     testnet: true,
 } as const satisfies Chain
