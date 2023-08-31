7b03c1c7d6cdd54a18565167620283188f6822f4
Dzmitry Baranau <dimabaranov98@gmail.com>
2023-08-31 13:42:29 +0300

    feat(dogechainTestnet): added DogeChain Testnet chain

---
+++ packages/chains/src/dogechainTestnet.ts
@@ -0,0 +1,26 @@
+import { Chain } from './types'
+
+export const dogechainTestnet = {
+  id: 568,
+  name: 'Dogechain Testnet',
+  network: 'dogechain-testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Testnet Doge',
+    symbol: 'tDC',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc-testnet.dogechain.dog'] },
+    public: { http: ['https://rpc-testnet.dogechain.dog'] },
+  },
+  blockExplorers: {
+    etherscan: {
+      name: 'TestnetDogeChainExplorer',
+      url: 'https://explorer-testnet.dogechain.dog',
+    },
+    default: {
+      name: 'TestnetDogeChainExplorer',
+      url: 'https://explorer-testnet.dogechain.dog',
+    },
+  },
+} as const satisfies Chain
